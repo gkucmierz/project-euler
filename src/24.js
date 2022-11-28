973419c2b5f3e6cec11767f85e8992c33da00dc9
Grzegorz Kućmierz <gkucmierz@gmail.com>
2022-11-28 10:42:58 +0100

    24

---
+++ src/24.js
@@ -0,0 +1,20 @@
+
+// https://projecteuler.net/problem=24
+
+const genPermutations = size => {
+  const perms = [];
+  const gen = (res, used, shift) => {
+    if (used === size) {
+      perms.push(res);
+    }
+    for (let i = 0; i < (size-used); ++i) {
+      gen([...res, shift[i]], used + 1, [...shift.slice(0, i), ...shift.slice(i + 1)]);
+    }
+  };
+  gen([], 0, new Array(size).fill(0).map((_, i) => i));
+  return perms;
+};
+
+console.log(
+  +genPermutations(10)[1e6-1].join('') // 2783915460
+);
