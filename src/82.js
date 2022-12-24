08c945b9d448d9fdd4d4fb166f314ab6f0658384
Grzegorz Kućmierz <gkucmierz@gmail.com>
2022-12-25 00:39:46 +0100

    83: optimize solution

--- src/82.js
+++ src/82.js
@@ -1,5 +1,6 @@
 
-// https://projecteuler.net/problem=83
+// https://projecteuler.net/problem=82
+// based on 83 solution
 
 const data = `
 4445,2697,5115,718,2209,2212,654,4348,3079,6821,7668,3276,8874,4190,3785,2752,9473,7817,9137,496,7338,3434,7152,4355,4552,7917,7827,2460,2350,691,3514,5880,3145,7633,7199,3783,5066,7487,3285,1084,8985,760,872,8609,8051,1134,9536,5750,9716,9371,7619,5617,275,9721,2997,2698,1887,8825,6372,3014,2113,7122,7050,6775,5948,2758,1219,3539,348,7989,2735,9862,1263,8089,6401,9462,3168,2758,3748,5870
@@ -100,42 +101,44 @@ const W = matrix[0].length;
 const getIdx = (h, w) => h * H + w;
 const best = new Array(H*W).fill(Infinity);
 
-const queue = [[H-1, W-1, 0]];
-const getVal = (h, w) => {
-  if (h < 0 || h >= H) return null;
-  if (w < 0 || w >= W) return null;
-  return matrix[h][w];
+const countRow = row => {
+  const queue = [[row, W-1, 0]];
+  
+  let cnt = 0;
+  const iter = (h, w, path = 0) => {
+    if (cnt++ % 1e5 === 0) console.log(cnt-1, queue.length);
+    if (h < 0 || h >= H) return;
+    if (w < 0 || w >= W) return;
+    path += matrix[h][w];
+    const idx = getIdx(h, w);
+    if (best[idx] <= path) return;
+    best[idx] = path;
+    [
+      [-1, 0],
+      [ 1, 0],
+      [ 0,-1],
+      [ 0, 1],
+    ].map(([hd, wd]) => queue.push([h + hd, w + wd, path]));
+  };
+  
+  do {
+    const args = queue.shift();
+    if (!args) break;
+    iter(...args);
+  } while (1);
+
+  let min = Infinity;
+  for (let i = 0; i < H; ++i) {
+    const idx = getIdx(i, 0);
+    if (best[idx] < min) min = best[idx];
+  }
+  return min;
 };
 
-// let cnt = 0;
-const iter = (h, w, path = 0) => {
-  // if (cnt++ % 1e5 === 0) console.log(cnt-1, queue.length);
-  if (h < 0 || h >= H) return;
-  if (w < 0 || w >= W) return;
-  path += matrix[h][w];
-  const idx = getIdx(h, w);
-  if (best[idx] <= path) return;
-  best[idx] = path;
-  ([
-    [-1, 0],
-    [ 1, 0],
-    [ 0,-1],
-    [ 0, 1],
-  ]).map(([hd, wd]) => [h + hd, w + wd])
-    .map(([hs, ws]) => {
-      const val = getVal(hs, ws);
-      return { hs, ws, val };
-    })
-    .filter(({val}) => val !== null)
-    .sort((a, b) => a.val - b.val)
-    .map(obj => queue.push([obj.hs, obj.ws, path]));
-};
+let min = Infinity;
+for (let i = 0; i < H; ++i) {
+  const val = countRow(i);
+  if (val < min) min = val;
+}
 
-do {
-  const args = queue.shift();
-  if (!args) break;
-  iter(...args);
-} while (1);
-
-console.log(best[0]); // 425185
-
+console.log(min);
