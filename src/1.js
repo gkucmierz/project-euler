7344e6cbfc564a5eba59c022242b81e97a27c581
Grzegorz Kućmierz <gkucmierz@gmail.com>
2023-04-07 23:08:45 +0100

    add another solution for 1 as comment

--- src/1.js
+++ src/1.js
@@ -9,3 +9,13 @@ for (let i = 0;i < 1e3; ++i) {
 }
 
 console.log(sum); // 233168
+
+
+// formula's solution:
+//
+// max = 999;
+// a = (max / 5 | 0) * (max / 5 + 1 | 0) / 2 * 5;
+// b = (max / 3 | 0) * (max / 3 + 1 | 0) / 2 * 3;
+// c = (max / 15 | 0) * (max / 15 + 1 | 0) / 2 * 15;
+//
+// a+b-c
